99ce7f087d06080478add135cffd8875b30c46e3
Dmitry <dean.grimm@yandex.ru>
2023-01-22 21:25:52 +0800

    задача 60

--- HomeWORK/task60/Program.cs
+++ HomeWORK/task60/Program.cs
@@ -1,10 +1,10 @@
-﻿// Задача 60. ...Сформируйте трёхмерный массив из уникальных двузначных чисел. 
-// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-// Массив размером 2 x 2 x 2
-// 66(0,0,0) 25(0,1,0)
-// 34(1,0,0) 41(1,1,0)
-// 27(0,0,1) 90(0,1,1)
-// 26(1,0,1) 55(1,1,1)
+﻿// // Задача 60. ...Сформируйте трёхмерный массив из уникальных двузначных чисел. 
+// // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// // Массив размером 2 x 2 x 2
+// // 66(0,0,0) 25(0,1,0)
+// // 34(1,0,0) 41(1,1,0)
+// // 27(0,0,1) 90(0,1,1)
+// // 26(1,0,1) 55(1,1,1)
 
 Console.WriteLine($"Введите размер массива n: ");
 int n = Convert.ToInt32(Console.ReadLine());
@@ -16,17 +16,40 @@ Console.WriteLine();
 
 void FillArray(int[,,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+  int[] temp = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
         {
-            for (int k = 0; k < array.GetLength(2); k++)
-            {
-                array[i,j,k] = new Random().Next(10,100);
-            }
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
         }
+          number = temp[i];
+      }
     }
+  }
+  int count = 0; 
+  for (int x = 0; x < array.GetLength(0); x++)
+  {
+    for (int y = 0; y < array.GetLength(1); y++)
+    {
+      for (int z = 0; z < array.GetLength(2); z++)
+      {
+        array[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
 }
+
 void PrintArray(int[,,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -39,5 +62,6 @@ void PrintArray(int[,,] array)
             }
             Console.WriteLine();
         }
+        Console.WriteLine();
     }
 }
